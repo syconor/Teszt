f3a25a245bac9bb6b394774e255caf73a942d1a9
syconor <39724736+syconor@users.noreply.github.com>
2018-07-19 23:06:15 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -12,17 +12,64 @@ const server = require('http')
 wsServer = new wssc( { httpServer: server, autoAcceptConnections: false } )
 
 var ct = []
+var id1 = 0
+var id2 = 0
+var x =0
 
 wsServer.on( 'request', r => {
         ct.push( r.accept('echo-protocol',r.origin) )
         ct[ct.length - 1].on( 'message', m => {
             let msg = JSON.parse(m.utf8Data)
-            console.log(msg);
-            ct.forEach( (v,k) => v.sendUTF(
-              msg.id + ':' + msg.msg
-            ) )
-        } )
+            perc=new Date().getMinutes()
+            ora=new Date().getHours()
+            sec=new Date().getSeconds()
+            console.log(m.utf8Data);
+            ct.forEach( (v,k) => v.sendUTF(msg.id + ':' + msg.msg) )
+            console.log(typeof msg.msg[0]);
+            //console.log(msg.id, msg.msg);
+            if (id1==0) {
+                  id1=msg.id
+            }
+            else {
+                  if (id2==0) {
+                    id2=msg.id
+                    if (id1==id2) {
+                      id2=0
+                    }
+                  }
+            }
+            console.log(id1,id2);
+
+            if (msg.msg[0]=="3") {
+              id1=0
+              id2=0
+              x=0
+              console.log(id1,id2);
+            }
+
+            if ((id1>0 && id2>0) && x==0) {
+                let q=Math.floor(Math.random())
+                x=1
+                if (q==0){
+                    msg.id=id1
+                    msg.msg = "50;50"
+                    ct.forEach( (v,k) => v.sendUTF(msg.id + ':' + msg.msg) )
+                    msg.id=id2
+                    msg.msg = "51;51"
+                    ct.forEach( (v,k) => v.sendUTF(msg.id + ':' + msg.msg) )
+                }
+                else{
+                  msg.id=id2
+                  msg.msg = "50;50"
+                  ct.forEach( (v,k) => v.sendUTF(msg.id + ':' + msg.msg) )
+                  msg.id=id1
+                  msg.msg = "51;51"
+                  ct.forEach( (v,k) => v.sendUTF(msg.id + ':' + msg.msg) )
+                }
+            }
+        })
 } )
+
 
 server.listen(3000, () => {
     console.log(`
